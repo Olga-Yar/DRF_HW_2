bf903a48326ea7c305242c8d0a8e0c524bed7d0c
Olga Yarosh <O_ll_y_a@mail.ru>
2023-08-21 20:14:28 +0300

    создание ограничения

--- study/views/course.py
+++ study/views/course.py
@@ -1,9 +1,12 @@
 # для курса через ViewSets
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
+from study.permissions import IsModerator, IsOwner
 from study.seriallizers.course import CourseDetailSerializer, Course
 
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseDetailSerializer
+    permission_classes = [IsAuthenticated, IsModerator|IsOwner]
